ed7fb70682ed732d6eaa35e55331fd61f9e1e00a
dhruvgcoder <dhruvgcoder@gmail.com>
2024-12-14 13:35:37 +0000

    Practice Containers

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12432
+let accountEmail = "contact@dhruvgupta.tech"
+var accountPassword = "12345678"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 45446 --> not allowed
+accountPassword = "987987"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/* Prefer not to use var because of 
+issue in block scope and functional scope */ 
