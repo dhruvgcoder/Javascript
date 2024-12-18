be1b771df274380ee7053548820e285b640f433a
Dhruv Gupta <dhruvgcoder@gmail.com>
2024-12-18 05:15:48 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = true
+
+
+//console.log(typeof score);
+//console.log(typeof(score)); // 2 method h likhne k 
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// score value => output
+//        "55" => 55
+//     "vb76b" => NaN
+//      "true" => 1 ; "false" => 0 
+
+let isLoggedIn = "Dhruv"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "Dhruv" => true
+// "" => false (empty string m false aata h else true )
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
