0aa745693770de2b6240e91d45b3337843d9bf3e
Dhruv Gupta <dhruvgcoder@gmail.com>
2024-12-18 04:55:15 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all JS code as Newer version
+
+/* alert(5+5) give error as we r using
+nodejs , not browser */
+
+// console.log(3+3); console.log("Dhruv") => not good practice 
+
+// code readability must be high 
+console.log(3+3);
+console.log("Dhruv");
+
+let name = "Dhruv"
+let age = 20
+let isLoggedIn = false
+let temp = null 
+
+// number => 2 to power 53
+// bigint
+// string ==> ""
+// boolean => true/false
+// null => standalone value 
+// undefined ( abi value assign nhi hui h )
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null); // null ka datatype object h 
