de297e79bd9151e829c49afc4e40d0ab57134f1e
Dhruv Gupta <dhruvgcoder@gmail.com>
2024-12-25 00:21:28 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -11,4 +11,3 @@ console.log(accountId);
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 /* Prefer not to use var because of 
 issue in block scope and functional scope */ 
-//Test Change
